807422cd7bd46da7825a5277488dde9defab7ac1
Dustin Lennon <dlennon@bestunion.com>
2018-07-20 10:38:35 -0400

    Add site command to start/stop pm2

---
+++ commands/site.js
@@ -0,0 +1,44 @@
+const {
+  Command
+} = require('discord-akairo')
+const {
+  spawn
+} = require('child_process')
+
+module.exports = class extends Command {
+  constructor() {
+    super('site', {
+      aliases: ['site', 's'],
+      args: [{
+        id: 'option',
+        type: ['start', 'stop'],
+        prompt: {
+          start: 'Are you needing to start or stop pm2? (start, stop)',
+          retry: 'Valid options are (start, stop)'
+        }
+      }],
+      ownerOnly: true
+    })
+  }
+
+  async exec(message, args) {
+    const pm2 = spawn('pm2', `${args.option}`, 'Dexcom')
+
+
+    switch (args.option) {
+      case 'start':
+        pm2.stdout.on('data', (data) => {
+          console.log('Data: ', data)
+        })
+
+        pm2.stderr.on('data', (data) => {
+          console.error('Data: ', data)
+        })
+        break
+
+      case 'stop':
+
+        break
+    }
+  }
+}
